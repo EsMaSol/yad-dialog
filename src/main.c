212086ae3eacf0729c11c0fb8ca229cbe7ad4fc2
ananasik <ananasik@d95e66fd-9402-4a7b-af37-a488d08096bb>
2009-12-02 10:42:06 +0000

    fix build on oldest gtk+ (<2.14)

--- src/main.c
+++ src/main.c
@@ -70,8 +70,12 @@ create_dialog ()
 
   /* add top label widgets */
   hbox = gtk_hbox_new (FALSE, 2);
+#if GTK_CHECK_VERSION (2, 14, 0)  
   gtk_container_add (GTK_CONTAINER (gtk_dialog_get_content_area 
 				    (GTK_DIALOG (dlg))), hbox);
+#else
+  gtk_container_add (GTK_CONTAINER (GTK_DIALOG (dlg)->vbox), hbox);
+#endif				    
   vbox = gtk_vbox_new (FALSE, 2);
   gtk_box_pack_end (GTK_BOX (hbox), vbox, TRUE, TRUE, 2);
 
