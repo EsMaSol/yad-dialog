19b8187073daedd5d23374bd3c80903050828dab
ananasik <ananasik@d95e66fd-9402-4a7b-af37-a488d08096bb>
2010-07-05 03:18:30 +0000

    update license information

--- src/about.c
+++ src/about.c
@@ -1,3 +1,23 @@
+/*
+ * This file is part of YAD.
+ *
+ * YAD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YAD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with YAD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Copyright (C) 2008-2010, Victor Ananjevsky <ananasik@gmail.com>
+ *
+ */
 
 #include "yad.h"
 
@@ -21,14 +41,14 @@ yad_about (void)
       "GNU General Public License for more details.\n\n"
       "You should have received a copy of the GNU General Public License "
       "along with this program; if not, write to the Free Software "
-      "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.");
+      "Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.");
   
   dialog = gtk_about_dialog_new ();
   
   g_object_set (G_OBJECT (dialog),
 		"name", PACKAGE_NAME,
 		"version", PACKAGE_VERSION,
-		"copyright", "Copyright \xc2\xa9 2009 Victor Ananjevsky <ananasik@gmail.com>",
+		"copyright", "Copyright \xc2\xa9 2008-2010 Victor Ananjevsky <ananasik@gmail.com>",
 		"comments", _("Yet Another Dialog\n(show dialog boxes from shell scripts)\n\nBased on Zenity code\n"),
 		"authors", authors,
 		"website", PACKAGE_URL,
