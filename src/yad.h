df566901da99d84551d23441c1726c2243f4de5c
ananasik <ananasik@d95e66fd-9402-4a7b-af37-a488d08096bb>
2011-01-10 08:03:29 +0000

    add alignment to form fields labels. prepare to the next 0.8.0 release

--- src/yad.h
+++ src/yad.h
@@ -1,3 +1,23 @@
+/*
+ * This file is part of YAD.
+ *
+ * YAD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YAD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with YAD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Copyright (C) 2008-2011, Victor Ananjevsky <ananasik@gmail.com>
+ *
+ */
 
 #ifndef _YAD_H_
 #define _YAD_H_
@@ -132,6 +152,7 @@ typedef struct {
 
 typedef struct {
   GSList *fields;
+  gdouble align;
 } YadFormData;
 
 typedef struct {
