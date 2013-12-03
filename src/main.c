f0e72bb9918c296d6d3e38e796cefb120a774d04
ananasik <ananasik@d95e66fd-9402-4a7b-af37-a488d08096bb>
2013-12-03 10:43:22 +0000

    set normal windows hints for yad windows

--- src/main.c
+++ src/main.c
@@ -131,6 +131,7 @@ create_dialog (void)
 
   /* create dialog window */
   dlg = gtk_dialog_new ();
+  gtk_window_set_type_hint (GTK_WINDOW (dlg), GDK_WINDOW_TYPE_HINT_NORMAL);
   gtk_window_set_title (GTK_WINDOW (dlg), options.data.dialog_title);
 #if !GTK_CHECK_VERSION(2,22,0)
   gtk_dialog_set_has_separator (GTK_DIALOG (dlg), options.data.dialog_sep);
