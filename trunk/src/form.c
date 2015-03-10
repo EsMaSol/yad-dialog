0db5f510ef1869e65275a2ab5849022acf6af524
v1c0nt <v1c0nt@d95e66fd-9402-4a7b-af37-a488d08096bb>
2015-03-10 10:44:44 +0000

    fix output by row in form dialog

--- trunk/src/form.c
+++ trunk/src/form.c
@@ -1131,10 +1131,11 @@ form_print_result (void)
     {
       guint j, rows;
 
-      rows = n_fields / options.form_data.columns + 1;
+      rows = n_fields / options.form_data.columns;
+      rows += (n_fields % options.form_data.columns ? 1 : 0);
       for (i = 0; i < rows; i++)
         {
-          for (j = 0; j <= options.form_data.columns; j++)
+          for (j = 0; j < options.form_data.columns; j++)
             {
               guint fld = i + rows * j;
               if (fld < n_fields)
